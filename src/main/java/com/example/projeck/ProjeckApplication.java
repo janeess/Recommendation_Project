f295e12e43e78d9f2bc16dd17e84223e09832699
janeess <sjs8739@naver.com>
2023-05-31 16:47:09 +0900

    first

---
+++ src/main/java/com/example/projeck/ProjeckApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjeckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjeckApplication.class, args);
+    }
+
+}
