d3e4b468b827173f86a7f02c529a5d194a64bbf6
janeess <sjs8739@naver.com>
2023-06-01 16:13:45 +0900

    2023.06.01

---
+++ src/main/java/com/example/project/recommendation/entity/Recommendation.java
@@ -0,0 +1,2 @@
+package com.example.project.recommendation.entity;public class Recommendation {
+}
---
+++ src/test/groovy/com/example/project/recommendation/repository/RecommendRepositoryTest.groovy
@@ -0,0 +1,4 @@
+package com.example.project.recommendation.repository
+
+class RecommendRepositoryTest {
+}
